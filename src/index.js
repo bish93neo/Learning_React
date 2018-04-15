eb1a940bc2e69550f184f00ffc262a495661885d
bish93neo <38405435+bish93neo@users.noreply.github.com>
2018-04-16 01:58:02 +0530

    commit_2
    youtube video app - loading first video

--- src/index.js
+++ src/index.js
@@ -1,8 +1,30 @@
 import React from "react";
 import ReactDOM from "react-dom";
-const App = () => {
+import YTSearch from "youtube-api-search";
+import SearchBar from "./components/search_bar";
+import VideoList from "./components/video_list";
+import VideoDetail from "./components/video_detail";
 
-	return <div>Hi!</div>;
+const API_KEY = "AIzaSyAkOvYnovo7iTMziTHOG3lodADk-Yo8qfE";
+
+class App extends React.Component {
+	constructor(props){
+		super(props);
+		this.state = {videos : []};
+
+		YTSearch({key : API_KEY, term : "northeastern"}, (videos) => {
+			this.setState({videos});
+		});
+	}
+	render(){
+		return (
+			<div>
+				<SearchBar />
+				<VideoDetail video = {this.state.videos[0]}/>
+				<VideoList videos = {this.state.videos}/>
+			</div>
+		);
+	}
 }
 
 ReactDOM.render(<App />, document.querySelector('.container'));
